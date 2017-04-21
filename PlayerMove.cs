edaae48af170e7397e1f3bbac6a6045814b23acf
Nick0108 <zidonglin@outlook.com>
2017-04-21 09:38:18 +0800

    refactor: Complete the Playermove with simple input

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -2,60 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
 
     public int MoveSpeed = 2;
     public Animator PlayerAnimator;
 
-    private Transform playerTransform;
-
-    void crossMove()
+    void CrossMove()
     {
-        playerTransform = transform;
-
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-            playerTransform.position += Vector3.forward * MoveSpeed * Time.deltaTime;
+            PlayerAnimator.SetBool("isWalk", true);
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.back * MoveSpeed / 2 * Time.deltaTime);
+            }
 
             if (Input.GetKey(KeyCode.A))
             {
-                playerTransform.position += Vector3.left * MoveSpeed * Time.deltaTime;
-                //transform.Translate((Vector3.forward + Vector3.left) * MoveSpeed * Time.deltaTime);
+                transform.Translate(Vector3.left * MoveSpeed * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                playerTransform.position += Vector3.right * MoveSpeed * Time.deltaTime;
-                //transform.Translate((Vector3.forward + Vector3.right) * MoveSpeed * Time.deltaTime);
-            }
-        } else if (Input.GetKey(KeyCode.S))
-        {
-            playerTransform.position += Vector3.back * MoveSpeed * Time.deltaTime;
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                playerTransform.position += Vector3.left * MoveSpeed * Time.deltaTime;
-                //transform.Translate((Vector3.forward + Vector3.left) * MoveSpeed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                playerTransform.position += Vector3.right * MoveSpeed * Time.deltaTime;
-                //transform.Translate((Vector3.forward + Vector3.right) * MoveSpeed * Time.deltaTime);
+                transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
             }
         }
         else
         {
-            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+            PlayerAnimator.SetBool("isWalk", false);
         }
-        
-     }
+    }
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		crossMove();
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CrossMove();
+    }
 }
