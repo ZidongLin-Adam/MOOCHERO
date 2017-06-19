44789b3924923b0fcfc5b3111b0cafd20f19046d
Nick0108 <zidonglin@outlook.com>
2017-06-19 20:42:11 +0800

    feat: Add the switchTeam

--- RoomPanelController.cs
+++ RoomPanelController.cs
@@ -93,6 +93,31 @@ public class RoomPanelController : PunBehaviour {
 		}
 	}
 
+	void UpdateTeamPanel(bool isUpdateSelf){
+		GameObject go;
+		foreach (PhotonPlayer p in PhotonNetwork.playerList) {
+			if (!isUpdateSelf && p.isLocal)	continue;			
+			costomProperties = p.customProperties;				
+			if (costomProperties ["Team"].Equals ("Team1")) {	
+				go = Team1 [(int)costomProperties ["TeamNum"]];	
+				go.SetActive (true);							
+				texts = go.GetComponentsInChildren<Text> ();	
+			} else {											
+				go = Team2 [(int)costomProperties ["TeamNum"]];	
+				go.SetActive (true);
+				texts = go.GetComponentsInChildren<Text> ();
+			}
+			texts [0].text = p.name;						
+			if(p.isMasterClient)							
+				texts[1].text="房主";						
+			else if ((bool)costomProperties ["isReady"]) {	
+				texts [1].text = "已准备";					
+			} else
+				texts [1].text = "未准备";					
+		}
+	}
+
+
 	void ReadyButtonControl(){
 		if (PhotonNetwork.isMasterClient) {									
 			readyButton.GetComponentInChildren<Text> ().text = "开始游戏";	
@@ -112,7 +137,52 @@ public class RoomPanelController : PunBehaviour {
 		}
 	}
 
-	
+	public void ClickSwitchButton(){
+		costomProperties = PhotonNetwork.player.customProperties;	
+		if ((bool)costomProperties ["isReady"]) {					
+			promptMessage.text="准备状态下不能切换队伍";				
+			return;												
+		}
+		bool isSwitched = false;		
+		if (costomProperties ["Team"].ToString ().Equals ("Team1")) {				
+			for (int i = 0; i < teamSize; i++) {									
+				if (!Team2 [i].activeSelf) {									
+					isSwitched = true;												
+					Team1 [(int)costomProperties ["TeamNum"]].SetActive (false);	
+					texts = Team2 [i].GetComponentsInChildren<Text> ();				
+					texts [0].text = PhotonNetwork.playerName;						
+					if(PhotonNetwork.isMasterClient)texts[1].text="房主";			
+					else texts [1].text = "未准备";									
+					Team2 [i].SetActive (true);										
+					costomProperties = new ExitGames.Client.Photon.Hashtable ()		
+					{ { "Team","Team2" }, { "TeamNum",i } };
+					PhotonNetwork.player.SetCustomProperties (costomProperties);	
+					break;
+				}
+			}
+		} else if (costomProperties ["Team"].ToString ().Equals ("Team2")) {		
+			for (int i = 0; i < teamSize; i++) {								
+				if (!Team1 [i].activeSelf) {										
+					isSwitched = true;												
+					Team2 [(int)(costomProperties ["TeamNum"])].SetActive (false);	
+					texts = Team1 [i].GetComponentsInChildren<Text> ();				
+					texts [0].text = PhotonNetwork.playerName;						
+					if(PhotonNetwork.isMasterClient)texts[1].text="房主";			
+					else texts [1].text = "未准备";								
+					Team1 [i].SetActive (true);									
+					costomProperties = new ExitGames.Client.Photon.Hashtable ()	
+					{ { "Team","Team1" }, { "TeamNum",i } };
+					PhotonNetwork.player.SetCustomProperties (costomProperties);
+					break;
+				}
+			}
+		}
+		if (!isSwitched)
+			promptMessage.text = "另一队伍已满,无法切换";	
+		else
+			promptMessage.text = "";
+	}
+
 	public void ClickReadyButton(){
 		bool isReady = (bool)PhotonNetwork.player.customProperties ["isReady"];				
 		costomProperties = new ExitGames.Client.Photon.Hashtable (){ { "isReady",!isReady } };
@@ -137,5 +207,8 @@ public class RoomPanelController : PunBehaviour {
 	}
 
 
-	
+	[PunRPC]
+	public void LoadGameScene(string sceneName){	
+		PhotonNetwork.LoadLevel (sceneName);	
+	}
 }
