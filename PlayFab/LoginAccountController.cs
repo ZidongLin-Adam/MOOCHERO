ed83aeebe69b1de408d266f94d56aa1b6303bbab
Nick0108 <zidonglin@outlook.com>
2017-08-09 20:17:40 +0800

    Feat: Add the Login controll function

---
+++ PlayFab/LoginAccountController.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+
+//玩家账号登录控制器
+public class LoginAccountController: MonoBehaviour {
+
+	public GameObject loginMainPanel;       //主登录面板
+	public GameObject loginAccountPanel;    //账号登录面板
+	public GameObject registerPanel;        //注册面板
+	public InputField account;              //玩家账号/邮箱输入栏
+	public InputField password;             //登录密码
+	public Text errorMessage;               //错误提示信息
+	public GameObject loginingWindow;       //“账号登录中”提示窗口
+
+    public GameObject loginPanel;           //登录面板
+	public GameObject mainPanel;            //游戏主面板
+
+    //玩家账号登录面板启动后调用，初始化界面信息
+	void OnEnable(){
+		errorMessage.text = "";
+        account.text = PlayerPrefs.GetString("Account", "");    //获取玩家上次登录成功的账号信息
+		password.text = "";
+		loginingWindow.SetActive(false);
+	}
+
+    //“登录”按钮响应事件
+	public void ClickLoginButton(){
+        /* 学生作业：实现玩家账号/邮箱登录的功能
+         * 作业提示：
+         * 首先，启用UI，提示玩家账号正在登录
+         * 其次在游戏客户端检测一些简单的非法输入
+         * 最后，使用PlayFab的Client API实现玩家账号/邮箱登录
+         * 账号登录：先使用LoginWithPlayFabRequest声明账号登录请求，在使用PlayFabClientAPI.LoginWithPlayFab发起请求；
+         * 邮箱登录：先使用LoginWithEmailAddressRequest声明账号登录请求，在使用PlayFabClientAPI.LoginWithEmailAddress发起请求。
+         * 登录成功，调用OnLoginResult函数，连接Photon服务器，游戏进入游戏主面板
+         * 登录失败，根据错误类型提示玩家登录失败原因，在控制台输出错误信息
+         */
+        //在游戏客户端检测一些简单的非法输入
+        if (account.text == "")
+            errorMessage.text = "请输入账号/邮箱";
+        else if(password.text == "")
+            errorMessage.text = "请输入密码";
+        else
+        {
+            //非法输入通过后
+            errorMessage.text = "";
+            //启用UI，提示玩家账号正在登录
+            loginingWindow.GetComponentInChildren<Text>().text = "账 号 登 录 中 ...";
+            loginingWindow.SetActive(true);
+            //最后，使用PlayFab的Client API实现玩家账号/邮箱登录
+            //邮箱登录：先使用LoginWithEmailAddressRequest声明账号登录请求，在使用PlayFabClientAPI.LoginWithEmailAddress发起请求。
+            if (account.text.Contains("@"))
+            {
+                LoginWithEmailAddressRequest request = new LoginWithEmailAddressRequest()
+                {
+                    Email = account.text,
+                    Password = password.text
+                };
+                PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginResult, OnPlayFabError);//登录成功，调用OnLoginResult函数，连接Photon服务器，游戏进入游戏主面板;登录失败，根据错误类型提示玩家登录失败原因，在控制台输出错误信息
+            }
+            else//账号登录：先使用LoginWithPlayFabRequest声明账号登录请求，在使用PlayFabClientAPI.LoginWithPlayFab发起请求；
+            {
+                LoginWithPlayFabRequest request = new LoginWithPlayFabRequest()
+                {
+                    Username = account.text,
+                    Password = password.text
+                };
+                PlayFabClientAPI.LoginWithPlayFab(request, OnLoginResult, OnPlayFabError);//登录成功，调用OnLoginResult函数，连接Photon服务器，游戏进入游戏主面板;登录失败，根据错误类型提示玩家登录失败原因，在控制台输出错误信息
+            }
+        }
+    }
+
+    //账号登录成功后，保存玩家信息，连接Photon服务器
+    void OnLoginResult(LoginResult result){
+		PlayerPrefs.SetString ("Account", account.text);    //保存玩家的登录信息
+        //在PlayFab保存玩家信息
+        PlayFabUserData.playFabId = result.PlayFabId;
+		PlayFabUserData.username = account.text;
+		loginingWindow.SetActive(false);
+
+        //连接Photon服务器
+		PhotonNetwork.ConnectUsingSettings ("1.0");
+		PhotonNetwork.player.name = account.text;
+
+		loginPanel.SetActive (false);               //禁用游戏登录面板
+        mainPanel.SetActive(true);                  //启用游戏主面板
+	}
+
+    //登录请求失败时调用，根据错误类型提示玩家登录失败原因
+	void OnPlayFabError(PlayFabError error){
+		loginingWindow.SetActive(false);
+		Debug.Log ("Get an error:" + error.Error);
+		if (error.Error == PlayFabErrorCode.InvalidParams && (error.ErrorDetails.ContainsKey ("Username") || (error.ErrorDetails.ContainsKey("Email")))){
+			errorMessage.text = "账号/邮箱输入不符合规范";
+		} else if (error.Error == PlayFabErrorCode.InvalidParams && error.ErrorDetails.ContainsKey ("Password")) {
+			errorMessage.text = "登录密码长度应为6~24位";
+		} else if (error.Error == PlayFabErrorCode.AccountNotFound) {
+			errorMessage.text = "账号/邮箱不存在";
+		} else if (error.Error == PlayFabErrorCode.InvalidUsernameOrPassword || error.Error == PlayFabErrorCode.InvalidEmailOrPassword) {
+			errorMessage.text = "登录密码错误";
+		} else if (error.Error == PlayFabErrorCode.AccountBanned) {
+			errorMessage.text = "账号/邮箱已被锁定";
+		} else {
+			errorMessage.text = "未知错误";
+		}
+	}
+
+    //“注册”按钮的响应函数
+	public void ClickRegisterButton(){
+		loginAccountPanel.SetActive (false);
+		registerPanel.SetActive (true);
+	}
+
+    //“取消”按钮的响应函数
+	public void ClickCancelButton(){
+		loginAccountPanel.SetActive (false);
+		loginMainPanel.SetActive (true);
+	}
+}
--- PlayFab/LoginPanelController.cs
+++ PlayFab/LoginPanelController.cs
@@ -1,85 +1,114 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-using Photon;
+using PlayFab;
+using PlayFab.ClientModels;
+//登录面板控制器
+public class LoginPanelController : MonoBehaviour {
 
-public class LoginPanelController : PunBehaviour {
+	public GameObject loginMainPanel;       //登录主面板
+	public GameObject loginAccountPanel;    //账号登录面板
+	public GameObject registerPanel;        //注册面板
+	public GameObject loginingWindow;       //“登录中”提示窗口
 
-	public GameObject loginPanel;		//游戏登录面板
-	public GameObject userMessage;		//玩家昵称信息
-	public Button backButton;			//后退按钮
-	public GameObject lobbyPanel;		//游戏大厅面板
-	public GameObject roomPanel;		//游戏房间面板
-	public Text username;				//玩家昵称文本
-	public Text connectionState;		//网络连接状态
+	public GameObject serverIPPanel;        //服务器IP设置面板
 
-	//初始化，根据当前客户端连接状态，显示相应的游戏面板
-	void Start () {
-		//如果未连接Photon服务器
-		if (!PhotonNetwork.connected) {
-			SetLoginPanelActive ();								//启用游戏登录面板
-			username.text = PlayerPrefs.GetString ("Username");	//在本地保存玩家昵称
-		} 
-		//如果已连接Photon服务器
-		else
-			SetLobbyPanelActive ();	//启用游戏大厅面板
-		connectionState.text = "";	//初始化网络连接状态文本信息
+	public GameObject loginPanel;           //登录面板
+	public GameObject mainPanel;            //游戏主面板
+
+    //登录面板启用时调用，初始化面板的显示
+	void OnEnable(){
+		loginMainPanel.SetActive (true);
+		loginAccountPanel.SetActive (false);
+		registerPanel.SetActive (false);
+		loginingWindow.SetActive(false);
+		if (serverIPPanel != null) {
+			serverIPPanel.SetActive (false);
+		}
 	}
 
-//条件编译指令，只在Unity编辑器中（UNITY_EDITOR）编译此段代码
-//#if(UNITY_EDITOR)	
-	void Update(){		
-		//在游戏画面左下角显示当前的网络连接状态
-		connectionState.text = PhotonNetwork.connectionStateDetailed.ToString ();
-	}
-//#endif
-
-	//启用游戏登录面板
-	public void SetLoginPanelActive(){
-		loginPanel.SetActive (true);				//启用游戏登录面板
-		userMessage.SetActive (false);				//禁用玩家昵称信息
-		backButton.gameObject.SetActive (false);	//禁用后退按钮
-		lobbyPanel.SetActive (false);				//禁用游戏大厅面板
-		if(roomPanel!=null)
-			roomPanel.SetActive (false);				//禁用游戏房间面板
-	}
-	//启用游戏大厅面板
-	public void SetLobbyPanelActive(){				
-		loginPanel.SetActive (false);				//禁用游戏登录面板
-		userMessage.SetActive (true);				//启用玩家昵称信息
-		backButton.gameObject.SetActive (true);		//启用后退按钮
-		lobbyPanel.SetActive (true);				//启用游戏大厅面板
+    //“账号登录”按钮的响应函数
+	public void ClickAccoutLoginButton(){
+		loginMainPanel.SetActive (false);
+		loginAccountPanel.SetActive (true);
 	}
 
-	//"登录"按钮事件处理函数
-	public void ClickLogInButton(){							
-		SetLobbyPanelActive ();			//启用游戏大厅面板
-		//客户端连接Photon服务器，游戏版本标识符为“1.0”
-		if (!PhotonNetwork.connected)						
-			PhotonNetwork.ConnectUsingSettings ("1.0");		
-		//如果玩家未输入昵称，这里自动为其分配一个昵称
-		if (username.text == "")							
-			username.text = "游客" + Random.Range (1, 9999);
-		PhotonNetwork.player.name = username.text;			//设置玩家昵称
-		PlayerPrefs.SetString ("Username", username.text);	//将玩家昵称保存在本地
-	}
-	//"退出"按钮事件处理函数
-	public void ClickExitGameButton(){
-		Application.Quit ();			//退出游戏应用
+    //“设备登录”按钮的响应函数
+	public void ClickDeviceLoginButton(){
+        //发起设备（自定义ID）登录请求，使用设备的标识符作为登录的自定义ID
+		LoginWithCustomIDRequest request = new LoginWithCustomIDRequest () {
+			CustomId = SystemInfo.deviceUniqueIdentifier,
+			CreateAccount = true,       //如果用户不存在，就自动创建账户
+		};
+		PlayFabClientAPI.LoginWithCustomID (request, OnLoginResult, OnPlayFabError);
+        loginingWindow.GetComponentInChildren<Text>().text = "设 备 登 录 中...";
+        loginingWindow.SetActive(true);
+    }
+
+    //设备登录成功后调用此函数
+	void OnLoginResult(LoginResult result){
+		loginingWindow.SetActive(false);
+        //保存玩家的playfabid
+        PlayFabUserData.playFabId = result.PlayFabId;
+        
+        //连接Photon服务器
+		PhotonNetwork.ConnectUsingSettings ("1.0");
+		PhotonNetwork.player.name = "游客" + result.PlayFabId;
+		PlayFabUserData.username = PhotonNetwork.player.name;
+
+        //如果玩家登录的是新创建的账号
+		if (result.NewlyCreated) {
+			string displayName = "游客" + result.PlayFabId;
+            //为玩家设置昵称（DisplayName）
+			UpdateUserTitleDisplayNameRequest request = new UpdateUserTitleDisplayNameRequest () {
+				DisplayName = displayName
+			};
+			PlayFabClientAPI.UpdateUserTitleDisplayName (request, OnUpdateUserTitleDisplayName, OnPlayFabError);
+		} else {
+			loginPanel.SetActive (false);				//禁用游戏登录面板
+			mainPanel.SetActive (true);					//启用游戏主面板
+		}
 	}
 
-	/**覆写IPunCallback回调函数，当玩家进入游戏大厅时调用
-	 * 显示玩家昵称
-	 */
-	public override void OnJoinedLobby(){
-		userMessage.GetComponentInChildren<Text> ().text
-		= "欢迎，" + PhotonNetwork.player.name;
-	}
+    //玩家昵称设置成功时调用
+	void OnUpdateUserTitleDisplayName(UpdateUserTitleDisplayNameResult result){
 
-	/**覆写IPunCallback回调函数，当客户端断开与Photon服务器的连接时调用
-	 * 游戏画面返回游戏登录面板
-	 */
-	public override void OnConnectionFail(DisconnectCause cause){
-		SetLoginPanelActive ();
+        /*  学生作业 2-3：玩家创建账号成功时，为玩家添加默认枪支：AK47
+         *  作业提示：
+         *  先使用PurchaseItemRequest声明一个道具购买请求，再使用PlayFabClientAPI.PurchaseItem发起道具购买请求
+         *  道具购买成功，禁用游戏登录面板，启用游戏主面板（OnPurchaseItemRequest函数）
+         *  道具购买失败，在控制台输出失败原因（OnPlayFabError函数）
+         */
+        PurchaseItemRequest request = new PurchaseItemRequest()
+        {
+            CatalogVersion = PlayFabUserData.catalogVersion,
+            VirtualCurrency = "FR",
+            Price = 0,
+            ItemId = "AK47"
+        };
+        PlayFabClientAPI.PurchaseItem(request, OnPurchaseItemResult, OnPlayFabError);
+
+		loginPanel.SetActive(false);                //禁用游戏登录面板
+		mainPanel.SetActive(true);                  //启用游戏主面板
+
+    }
+
+    //道具购买成功时调用
+	void OnPurchaseItemResult(PurchaseItemResult result){
+        loginPanel.SetActive(false);                //禁用游戏登录面板
+        mainPanel.SetActive(true);                  //启用游戏主面板
+    }
+
+    //PlayFab请求出错时调用，在控制台输出错误信息
+    void OnPlayFabError(PlayFabError error){
+		Debug.LogError (error.ToString ());
+		loginingWindow.SetActive(false);
+	}
+    //点击“服务器IP设置”按钮，显示服务器IP设置面板
+	public void ClickServerIPButton(){
+		loginMainPanel.SetActive(false);
+		loginAccountPanel.SetActive(false);
+		registerPanel.SetActive(false);
+		serverIPPanel.SetActive (true);
 	}
 }
